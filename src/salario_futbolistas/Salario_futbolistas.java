dd3900d63d0a1d925954b3820d4cf579de848a9f
brandon <bscastro24@misena.edu.co>
2019-03-24 12:35:48 -0500

    capturar la ruta del archivo json

--- src/salario_futbolistas/Salario_futbolistas.java
+++ src/salario_futbolistas/Salario_futbolistas.java
@@ -2,12 +2,15 @@
 package salario_futbolistas;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
 import static oracle.jrockit.jfr.events.Bits.intValue;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -19,24 +22,38 @@ public class Salario_futbolistas {
 
     
     public static void main(String[] args) throws IOException, FileNotFoundException, ParseException {       
-        JSONArray  jsonarray = leer_json();
+        String ruta = ruta_de_json();
+        JSONArray  jsonarray = leer_json(ruta);
         HashMap<String, json> map = convercion_a_map(jsonarray);
+        System.out.println(map);        
 	}  
-    
+    public static String ruta_de_json(){   
+        System.out.println("copia la ruta del archivo json");
+        Scanner reader = new Scanner(System.in);
+        String ruta = reader.nextLine();
+        return ruta;        
+    }
     public static HashMap convercion_a_map (JSONArray jsonarray) throws IOException, FileNotFoundException, ParseException{
           JSONArray  jsonarray2 = jsonarray;
           JSONObject obj = new JSONObject(); 
-          HashMap<String, json> map = new HashMap<String, json>();          
+          HashMap<String, json> map = new HashMap<String, json>(); 
+          String nombre;
+          String nivel;
+          int goles;
+          int sueldo;
+          int bono;
+          int sueldo_completo;
+          String equipo;
            for(int i = 0;i<jsonarray2.size();i++){
            String clave = Integer.toString(i);
            obj = (JSONObject) jsonarray2.get(i); 
-           String nombre = (String) obj.get("nombre");
-           String nivel = (String) obj.get("nivel");           
-           int goles = intValue(obj.get("goles"));
-           int sueldo = intValue(obj.get("sueldo"));
-           int bono = intValue(obj.get("bono"));
-           int sueldo_completo = intValue(obj.get("sueldo_completo"));
-           String equipo = (String) obj.get("equipo");
+           nombre = (String) obj.get("nombre");
+           nivel = (String) obj.get("nivel");           
+           goles = intValue(obj.get("goles"));
+           sueldo = intValue(obj.get("sueldo"));
+           bono = intValue(obj.get("bono"));
+           sueldo_completo = intValue(obj.get("sueldo_completo"));
+           equipo = (String) obj.get("equipo");
            map.put(clave,new json(nombre,nivel,goles,sueldo,bono,sueldo_completo,equipo));            
         }  
         return map;                      
@@ -44,9 +61,9 @@ public class Salario_futbolistas {
          
     
     
-    public static JSONArray  leer_json() throws FileNotFoundException, IOException, ParseException{
+    public static JSONArray  leer_json(String ruta) throws FileNotFoundException, IOException, ParseException{
         JSONParser parser = new JSONParser();      
-	Object obj = parser.parse(new FileReader("informacion.json"));        
+	Object obj = parser.parse(new FileReader(ruta));        
 	JSONArray  jsonarray = (JSONArray ) obj;                        
         return jsonarray ;
             
